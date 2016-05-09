dd9be302ee2728997e76aedfaf7bd5191f9d6381
Philipp Wördehoff <ph.woerdehoff@gmail.com>
2016-05-09 13:30:22 +0200

    added typings

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,5 +1,7 @@
 /// <reference path="browser/ambient/bluebird/bluebird.d.ts" />
+/// <reference path="browser/ambient/bluebird/index.d.ts" />
 /// <reference path="browser/ambient/chai/chai.d.ts" />
+/// <reference path="browser/ambient/chai/index.d.ts" />
 /// <reference path="browser/ambient/mocha/mocha.d.ts" />
 /// <reference path="browser/ambient/node/node.d.ts" />
 /// <reference path="browser/ambient/redis/redis.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,5 +1,7 @@
 /// <reference path="main/ambient/bluebird/bluebird.d.ts" />
+/// <reference path="main/ambient/bluebird/index.d.ts" />
 /// <reference path="main/ambient/chai/chai.d.ts" />
+/// <reference path="main/ambient/chai/index.d.ts" />
 /// <reference path="main/ambient/mocha/mocha.d.ts" />
 /// <reference path="main/ambient/node/node.d.ts" />
 /// <reference path="main/ambient/redis/redis.d.ts" />
